0cf64f2337e8bd8452a0a7e21812007e234b0ca9
zjq1945@outlook.com <zjq1945@outlook.com>
2019-07-23 23:33:10 +0800

    code check in

--- MyFirstGoApi/firstApi_Gin.go
+++ MyFirstGoApi/firstApi_Gin.go
@@ -5,7 +5,7 @@ import "time"
 
 func main() {
 	r := gin.Default()
-	r.GET("/GetTimeNow", func(c *gin.Context) {
+	r.GET("JackyTest/GetTimeNow", func(c *gin.Context) {
 		c.JSON(200, gin.H{"Current Time": time.Now()})
 	})
 	r.Run(":8008")
